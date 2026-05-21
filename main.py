2edd1c3f024e69754640384482fa78dd5b9711be
Janette <janetterp2@gmail.com>
2026-05-21 20:46:30 +0200

    parsing map

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from parser import Parser_Error, MapParser
 def main() -> int:
 
     if (len(sys.argv) != 2):
-        sys.stderr.write("Usage: python3 main.py <maps/map_file.txt>")
+        sys.stderr.write("Usage: python3 main.py <maps/map_file.txt>\n")
         return 1
 
     map_path = sys.argv[1]
     try:
         parser = MapParser(map_path)
+        data_with_comments = parser.read_map()
+        data_no_comment = parser.clean_comments(data_with_comments)
+        data = parser.clean_next_line(data_no_comment)
+        print(data)
     except Parser_Error as e:
         print(f"{e}")
         return 1
--- parser.py
+++ parser.py
@@ -6,3 +6,27 @@ class Parser_Error(Exception):
 class MapParser():
     def __init__(self, map_path: str) -> None:
         self.map_path = map_path
+
+    def read_map(self) -> list[str]:
+        with open(self.map_path, "r", encoding="utf-8") as file:
+            data = file.readlines()
+        return data
+
+    def clean_comments(self, data: list[str]) -> list[str]:
+        for line in data:
+            if line.startswith('#'):
+                data.remove(line)
+        return data
+
+    def clean_next_line(self, data: list[str]) -> list[str]:
+        for i, line in enumerate(data):
+            data[i] = line.rstrip("\n")
+        return data
+
+    def clean_empty_lines(self, data: list[str]) -> list[str]:
+        for line in data:
+            if line == "":
+                data.remove(line)
+        return data
+
+# def read_map(map: MapParser):
